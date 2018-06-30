79522095afac102983aa3246d9e828f7bcb444c9
Mazi Muhlari <mazi.muhlari@gmail.com>
2018-06-30 16:52:30 +0200

    Updats for Keep score

--- routes/site.js
+++ routes/site.js
@@ -5,4 +5,8 @@ router.get('/', async (req, res) => {
     res.render('index.ejs');
 });
 
+router.get('/keep-score', async (req, res) => {
+    res.render('keep-score.ejs');
+});
+
 module.exports = router;
